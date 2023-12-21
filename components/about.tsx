40f267d7d6d508e5af8128065ddc6dad579a678a
nabeff <neffina8@gmail.com>
2023-12-21 21:19:18 +0100

    changed cv

--- components/about.tsx
+++ components/about.tsx
@@ -19,31 +19,32 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3 dark:text-white/70">
-        After graduating with a degree in{" "}
-        <span className="dark:text-white/80 font-bold">Computer Science</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="dark:text-white/80 font-bold">full-stack web development</span>.{" "}
-        <span className="italic dark:text-white/80 font-bold">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline dark:text-white/80 font-bold">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="dark:text-white/80 font-bold">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="dark:text-white/80 font-bold">full-time position</span> as a software
-        developer.
-      </p>
+      After graduating with a degree in{" "}
+      <span className="dark:text-white/80 font-bold">Computer Science</span>, I decided to pursue my
+      passion for programming. I enrolled in a coding bootcamp and learned{" "}
+      <span className="dark:text-white/80 font-bold">full-stack web development</span>.{" "}
+      <span className="italic dark:text-white/80 font-bold">My favorite part of programming</span> is the
+      problem-solving aspect. I{" "}
+      <span className="underline dark:text-white/80 font-bold">love</span> the
+      feeling of finally figuring out a solution to a problem. My core stack
+      is{" "}
+      <span className="dark:text-white/80 font-bold">
+        React, Next.js, Node.js, and MongoDB
+      </span>
+      . I am also familiar with TypeScript and Prisma. I am always looking to
+      learn new technologies. I am currently looking for a{" "}
+      <span className="dark:text-white/80 font-bold">full-time position</span> as a software
+      developer.
+    </p>
 
-      <p className="dark:text-white/70">
-        <span className="italic dark:text-white/80 font-bold">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my cat. I also enjoy{" "}
-        <span className="dark:text-white/80 font-bold">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium dark:text-white/80">history and philosophy</span>. I'm also
-        learning how to play the piano.
-      </p>
+    <p className="dark:text-white/70">
+      <span className="italic dark:text-white/80 font-bold">When I&apos;m not coding</span>, I enjoy playing
+      video games, watching movies, and playing with my cat. I also enjoy{" "}
+      <span className="dark:text-white/80 font-bold">learning new things</span>. I am currently
+      learning about{" "}
+      <span className="font-medium dark:text-white/80">history and philosophy</span>. I&apos;m also
+      learning how to play the piano.
+    </p>
       <div className="absolute top-1/2 left-1/2 transform blur-[13rem] -translate-x-1/2 
       -translate-y-1/2 bg-[#ff0000] rounded-full w-[13rem] h-[13rem] -z-10 dark:bg-[#ff0000] "></div>
       <div className="absolute top-1/2 right-[-35rem]  transform blur-[14rem] -translate-x-1/2 
--- components/intro.tsx
+++ components/intro.tsx
@@ -56,14 +56,15 @@ export default function Intro() {
         </div>
 
         <motion.p className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl dark:text-white'
-        initial={{ opacity:0, y:100}}
-        animate={{ opacity:1, y:0}}>
-        <span className="font-bold">Hello, I'm Nabil.</span> I'm a{" "}
-        <span className="font-bold">Front End developer</span> with{" "}
-        <span className="font-bold">years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>. 
-        </motion.p>
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+            >
+                <span className="font-bold">Hello, I&apos;m Nabil.</span> I&apos;m a{' '}
+                <span className="font-bold">Front End developer</span> with{' '}
+                <span className="font-bold">years</span> of experience. I enjoy
+                building <span className="italic">sites &amp; apps</span>. My focus is{' '}
+                <span className="underline">React (Next.js)</span>.
+            </motion.p>
 
         <motion.div className="flex flex-col sm:flex-row items-center
          justify-center gap-4 px-4 text-lg font-medium"
@@ -89,7 +90,7 @@ export default function Intro() {
              items-center gap-2 rounded-full outline-none 
              focus:scale-110 hover:scale-110 
               active:scale-105 transition cursor-pointer borderBlack
-              dark:bg-white/10' href="/Cv.pdf" download={true}>Download CV
+              dark:bg-white/10' href="/cv_nabileffina_eng.pdf" download={true}>Download CV
              <HiDownload className="opacity-60 group-hover:translate-x-1 transition"/>
              </a>
 
